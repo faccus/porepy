4449a3060d97ec4cbade57ad76b486ba6c3d5a3d
Jhabriel Varela <jhabriel@gmail.com>
2022-12-07 16:41:46 +0100

    MAINT: Renaming of files

--- src/porepy/models/applications/mandel_poroelasticity/mandel_runner.py
+++ src/porepy/models/applications/mandel_poroelasticity/mandel_runner.py
@@ -1,21 +1,21 @@
 """
-This module contains a runner for Terzaghi's consolidation problem using default
+This module contains a runner for Mandel's consolidation problem using default
 parameters.
 
 For an overview of the admissible parameters, please refer to the documentation of the
-``Terzaghi`` class available in ``porepy.models.applications.terzaghi.terzaghi_model``.
+``Mandel`` class available in ``porepy.models.applications.mandel.mandel_model``.
 
 """
 
 # Import modules
 import porepy as pp
 from time import time
-from porepy.models.applications.terzaghi.terzaghi_model import Terzaghi
+from porepy.models.applications.mandel_poroelasticity.mandel_model import Mandel
 
 # Runner
 tic = time()
 params = {"plot_results": True}
-setup = Terzaghi(params)
+setup = Mandel(params)
 print("Simulation started...")
 pp.run_time_dependent_model(setup, setup.params)
 toc = time()
--- src/porepy/models/applications/terzaghi_poroelasticity/terzaghi_runner.py
+++ src/porepy/models/applications/terzaghi_poroelasticity/terzaghi_runner.py
@@ -1,21 +1,23 @@
 """
-This module contains a runner for Mandel's consolidation problem using default
+This module contains a runner for Terzaghi's consolidation problem using default
 parameters.
 
 For an overview of the admissible parameters, please refer to the documentation of the
-``Mandel`` class available in ``porepy.models.applications.mandel.mandel_model``.
+``Terzaghi`` class available in ``porepy.models.applications.terzaghi.terzaghi_model``.
 
 """
 
 # Import modules
 import porepy as pp
 from time import time
-from porepy.models.applications.mandel.mandel_model import Mandel
+from porepy.models.applications.terzaghi_poroelasticity.terzaghi_model import Terzaghi
 
 # Runner
 tic = time()
-params = {"plot_results": True}
-setup = Mandel(params)
+params = {
+    "plot_results": True,
+}
+setup = Terzaghi(params)
 print("Simulation started...")
 pp.run_time_dependent_model(setup, setup.params)
 toc = time()
