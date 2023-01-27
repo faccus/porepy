db42054b2d03b9c0af4a361fb3dfaff94a6d0577
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-01-27 21:45:26 +0100

    WIP: Sneddon's verification

---
+++ src/porepy/applications/verification_setups/sneddon_contact_mech.py
@@ -0,0 +1,13 @@
+"""
+This module contains an implementation of Sneddon's crack pressurization problem.
+The problem consists of a one-dimensional crack of length :math:`2h` fully embedded in
+the center of a host rock, forming an angle :math:`theta` with the horizontal axis.
+
+A pressure :maht:`P` is exherted, equally, at both sides of the interior
+faces of the crack, causing a relative normal displacement :math:`[\mathbf{u}]_n` of
+the crack. Shear forces are zero.
+
+Sneddon's [1] found an exact solution for the relative displacement as a function of
+the :math:`P`, :
+
+"""
