8081d406fa28541c811761d54f293a11e0e08596
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-02-24 01:19:34 +0100

    Update geo_sneddon.py

--- src/porepy/experimental/geo_sneddon.py
+++ src/porepy/experimental/geo_sneddon.py
@@ -1,9 +1,12 @@
 import porepy as pp
 import numpy as np
 from porepy.fracs.gmsh_interface import GmshWriter
+from porepy.fracs.fracture_importer import dfm_from_gmsh
+from porepy.fracs.simplex import _read_gmsh_file
 import gmsh
+import sys
 
-
+#%%
 # Parameters for Sneddon mdg
 domain_size = (50, 50)
 crack_length = 20
@@ -48,101 +51,113 @@ x1_e1 = x1 + tip_radii * np.cos(crack_angle)
 y1_e1 = y1 + tip_radii * np.sin(crack_angle)
 
 # Let's try to write in the API
-if not GmshWriter.gmsh_initialized:
-    gmsh.initialize()
-    GmshWriter.gmsh_initialized = True
+# if not GmshWriter.gmsh_initialized:
+#     gmsh.initialize()
+#     GmshWriter.gmsh_initialized = True
 
+gmsh.initialize()
 gmsh.option.setNumber("General.Verbosity", 3)
+gmsh.model.add("sneddon_api")
 
 # ----> 0d-objects
 
-# Boundary points
-gmsh.model.geo.addPoint(0, 0, 0, meshSize=mesh_size, tag=1)
-gmsh.model.geo.addPoint(a, 0, 0, meshSize=mesh_size, tag=2)
-gmsh.model.geo.addPoint(a, b, 0, meshSize=mesh_size, tag=3)
-gmsh.model.geo.addPoint(0, b, 0, meshSize=mesh_size, tag=4)
-gmsh.model.geo.synchronize()
-
-pg_point_1 = gmsh.model.addPhysicalGroup(0, [1])
-pg_point_2 = gmsh.model.addPhysicalGroup(0, [2])
-pg_point_3 = gmsh.model.addPhysicalGroup(0, [3])
-pg_point_4 = gmsh.model.addPhysicalGroup(0, [4])
-
-gmsh.model.setPhysicalName(0, pg_point_1, "DOMAIN_BOUNDARY_POINT_1")
-gmsh.model.setPhysicalName(0, pg_point_2, "DOMAIN_BOUNDARY_POINT_2")
-gmsh.model.setPhysicalName(0, pg_point_3, "DOMAIN_BOUNDARY_POINT_3")
-gmsh.model.setPhysicalName(0, pg_point_4, "DOMAIN_BOUNDARY_POINT_4")
-
 # Fracture tips
-gmsh.model.geo.addPoint(x0, y0, 0, meshSize=mesh_size, tag=5)
-gmsh.model.geo.addPoint(x1, y1, 0, meshSize=mesh_size, tag=6)
+gmsh.model.geo.addPoint(x0, y0, 0, meshSize=mesh_size, tag=1)
+gmsh.model.geo.addPoint(x1, y1, 0, meshSize=mesh_size, tag=2)
 gmsh.model.geo.synchronize()
 
 # Circle endpoints
-gmsh.model.geo.addPoint(x0_e0, y0_e0, 0, meshSize=mesh_size, tag=7)
-gmsh.model.geo.addPoint(x0_e1, y0_e1, 0, meshSize=mesh_size, tag=8)
-gmsh.model.geo.addPoint(x1_e0, y1_e0, 0, meshSize=mesh_size, tag=9)
-gmsh.model.geo.addPoint(x1_e1, y1_e1, 0, meshSize=mesh_size, tag=10)
+gmsh.model.geo.addPoint(x0_e0, y0_e0, 0, meshSize=mesh_size, tag=3)
+gmsh.model.geo.addPoint(x0_e1, y0_e1, 0, meshSize=mesh_size, tag=4)
+gmsh.model.geo.addPoint(x1_e0, y1_e0, 0, meshSize=mesh_size, tag=5)
+gmsh.model.geo.addPoint(x1_e1, y1_e1, 0, meshSize=mesh_size, tag=6)
+gmsh.model.geo.synchronize()
+
+# Boundary points
+gmsh.model.geo.addPoint(0, 0, 0, meshSize=mesh_size, tag=7)
+gmsh.model.geo.addPoint(a, 0, 0, meshSize=mesh_size, tag=8)
+gmsh.model.geo.addPoint(a, b, 0, meshSize=mesh_size, tag=9)
+gmsh.model.geo.addPoint(0, b, 0, meshSize=mesh_size, tag=10)
+gmsh.model.geo.synchronize()
+
+pg_point_7 = gmsh.model.addPhysicalGroup(0, [7])
+pg_point_8 = gmsh.model.addPhysicalGroup(0, [8])
+pg_point_9 = gmsh.model.addPhysicalGroup(0, [9])
+pg_point_10 = gmsh.model.addPhysicalGroup(0, [10])
+
+gmsh.model.setPhysicalName(0, pg_point_7, "DOMAIN_BOUNDARY_POINT_6")
+gmsh.model.setPhysicalName(0, pg_point_8, "DOMAIN_BOUNDARY_POINT_7")
+gmsh.model.setPhysicalName(0, pg_point_9, "DOMAIN_BOUNDARY_POINT_8")
+gmsh.model.setPhysicalName(0, pg_point_10, "DOMAIN_BOUNDARY_POINT_9")
 
 # -----> 1d-objects
 
-# Boundary lines
-gmsh.model.geo.addLine(1, 2, tag=1)
-gmsh.model.geo.addLine(2, 3, tag=2)
-gmsh.model.geo.addLine(3, 4, tag=3)
-gmsh.model.geo.addLine(4, 1, tag=4)
-gmsh.model.geo.synchronize()
-
-pg_line_1 = gmsh.model.addPhysicalGroup(1, [1])
-pg_line_2 = gmsh.model.addPhysicalGroup(1, [2])
-pg_line_3 = gmsh.model.addPhysicalGroup(1, [3])
-pg_line_4 = gmsh.model.addPhysicalGroup(1, [4])
-
-gmsh.model.setPhysicalName(1, pg_line_1, "DOMAIN_BOUNDARY_LINE_1")
-gmsh.model.setPhysicalName(1, pg_line_2, "DOMAIN_BOUNDARY_LINE_2")
-gmsh.model.setPhysicalName(1, pg_line_3, "DOMAIN_BOUNDARY_LINE_3")
-gmsh.model.setPhysicalName(1, pg_line_4, "DOMAIN_BOUNDARY_LINE_4")
-
 # Fracture lines
-gmsh.model.geo.addLine(5, 8, tag=5)
-gmsh.model.geo.addLine(8, 9, tag=6)
-gmsh.model.geo.addLine(9, 6, tag=7)
+gmsh.model.geo.addLine(1, 4, tag=1)
+gmsh.model.geo.addLine(4, 5, tag=2)
+gmsh.model.geo.addLine(5, 2, tag=3)
 gmsh.model.geo.synchronize()
 
-pg_line_5 = gmsh.model.addPhysicalGroup(1, [5, 6, 7])
-gmsh.model.setPhysicalName(1, pg_line_5, "FRACTURE_1")
+pg_line_1 = gmsh.model.addPhysicalGroup(1, [1, 2, 3])
+gmsh.model.setPhysicalName(1, pg_line_1, "FRACTURE_0")
 
 # Arcs
-gmsh.model.geo.addCircleArc(7, 5, 8, tag=8)
-gmsh.model.geo.addCircleArc(8, 5, 7, tag=9)
-gmsh.model.geo.addCircleArc(9, 6, 10, tag=10)
-gmsh.model.geo.addCircleArc(10, 6, 9, tag=11)
+gmsh.model.geo.addCircleArc(3, 1, 4, tag=4)
+gmsh.model.geo.addCircleArc(4, 1, 3, tag=5)
+gmsh.model.geo.addCircleArc(5, 2, 6, tag=6)
+gmsh.model.geo.addCircleArc(6, 2, 5, tag=7)
 gmsh.model.geo.synchronize()
 
-pg_line_6 = gmsh.model.addPhysicalGroup(1, [8, 9])
-pg_line_7 = gmsh.model.addPhysicalGroup(1, [10, 11])
+pg_line_2 = gmsh.model.addPhysicalGroup(1, [4, 5])
+pg_line_3 = gmsh.model.addPhysicalGroup(1, [6, 7])
 
-gmsh.model.setPhysicalName(1, pg_line_6, "AUXILIARY_LINE_1")
-gmsh.model.setPhysicalName(1, pg_line_7, "AUXILIARY_LINE_2")
+gmsh.model.setPhysicalName(1, pg_line_2, "AUXILIARY_LINE_1")
+gmsh.model.setPhysicalName(1, pg_line_3, "AUXILIARY_LINE_2")
+
+# Boundary lines
+gmsh.model.geo.addLine(7, 8, tag=8)
+gmsh.model.geo.addLine(8, 9, tag=9)
+gmsh.model.geo.addLine(9, 10, tag=10)
+gmsh.model.geo.addLine(7, 10, tag=11)
+gmsh.model.geo.synchronize()
+
+pg_line_3 = gmsh.model.addPhysicalGroup(1, [8])
+pg_line_4 = gmsh.model.addPhysicalGroup(1, [9])
+pg_line_5 = gmsh.model.addPhysicalGroup(1, [10])
+pg_line_6 = gmsh.model.addPhysicalGroup(1, [11])
+
+gmsh.model.setPhysicalName(1, pg_line_3, "DOMAIN_BOUNDARY_LINE_7")
+gmsh.model.setPhysicalName(1, pg_line_4, "DOMAIN_BOUNDARY_LINE_8")
+gmsh.model.setPhysicalName(1, pg_line_5, "DOMAIN_BOUNDARY_LINE_9")
+gmsh.model.setPhysicalName(1, pg_line_6, "DOMAIN_BOUNDARY_LINE_10")
 
 # ----> Set loops, surfaces, and generate geo file
 
 # Set loop
-gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+gmsh.model.geo.addCurveLoop([8, 9, 10, -11], 1, reorient=True)
 gmsh.model.geo.synchronize()
 
 # Define surface
 gmsh.model.geo.addPlaneSurface([1], 1)
 gmsh.model.geo.synchronize()
 
-# Embed lower-dimensional objects
-gmsh.model.mesh.embed(1, [5, 6, 7, 8, 9, 10, 11], 2, 1)
+pg_surface_1 = gmsh.model.addPhysicalGroup(2, [1])
+gmsh.model.setPhysicalName(2, pg_surface_1, "DOMAIN")
 
-gmsh.write("sneddon_api.geo_unrolled")
-gmsh.clear()
+# Embed lower-dimensional objects
+gmsh.model.mesh.embed(1, [1, 2, 3, 4, 5, 6, 7], 2, 1)
+
+# -----> Mesh it
+gmsh.model.mesh.generate(2)
+gmsh.write("sneddon_api.msh")
+
+# if '-nopopup' not in sys.argv:
+#     gmsh.fltk.run()
+
 gmsh.finalize()
 
-
+#%%
+mdg = dfm_from_gmsh("sneddon_api.msh", dim=2)
 
 
 
